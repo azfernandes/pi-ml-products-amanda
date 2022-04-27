496793d4c4e5f743eb343197d214f39269298c84
Andre Veziane <andre.veziane@mercadolivre.com>
2022-04-27 14:25:51 -0300

    fix: changed the type of product id to wrapper

--- src/main/java/com/piml/products/entity/Product.java
+++ src/main/java/com/piml/products/entity/Product.java
@@ -17,7 +17,7 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "product_id")
-    private long id;
+    private Long id;
 
     private String name;
     private String description;
